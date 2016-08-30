2ccb47d6d30e61f0a1ccaf7b135b50cf540f4845
Chris Hicks <chicks@flow.io>
2016-08-30 14:35:34 -0400

    npm install while releasing

--- release.go
+++ release.go
@@ -8,6 +8,7 @@ func main() {
 	executor := executor.Create("node-sdk")
 	executor = executor.Add("git checkout master")
 	executor = executor.Add("git pull origin master")
+	executor = executor.Add("npm install")
 	executor = executor.Add("npm run generate")
 	executor = executor.Add("npm version --no-git-tag-version `node -p \"require('./api.service.json').service.version\"`")
 	executor = executor.Add("git add src docs package.json")
