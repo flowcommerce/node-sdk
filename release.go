c488ed186a14f16422dcded9569e503c354ecc25
Michael Palma <mpalma@flowio.io>
2019-05-02 10:09:05 -0400

    Adding package-lock to go script and syncing version

--- release.go
+++ release.go
@@ -10,7 +10,7 @@ func main() {
 	executor = executor.Add("git pull origin master")
 	executor = executor.Add("npm install")
 	executor = executor.Add("npm run generate")
-	executor = executor.Add("git add src docs package.json")
+	executor = executor.Add("git add src docs package.json package-lock.json")
 	executor = executor.Add("git commit -m 'autocommit: version and generated src and docs directories. API Version `node -p \"require('./api.service.json').service.version\"`'")
 	executor = executor.Add("dev tag")
 	executor = executor.Add("npm publish --otp")
